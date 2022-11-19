1d6e7323d224b50ec054324209dab6463fd8f241
middleware <malifarge.thomas9@gmail.com>
2022-11-19 17:14:32 +0100

    function php

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function hello($firstName, $lastName, int $fruit){
+    $fruits = ['apple','pear','banana'];
+    echo "<p>Hello $firstName $lastName , you love $fruits[$fruit] </p>";
+}
+
+function somme (int $a, int $b){
+    $Somme = $a + $b;
+    return $Somme;
+}
+
+?>
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-    $firstName= 'Jean';
+    $firstName= 'Thomas';
     $lastName= 'Malifarge';
     if($firstName === "Thomas"){
         echo "<h1>hello $firstName $lastName </h1>";
@@ -13,9 +13,16 @@
         echo "$i <br>";
     }
 
-    $fruits = ['pomme','poire','banane'];
+    $fruits = ['apple','pear','banana'];
 
     foreach ($fruits as &$fruit){
         echo "$fruit <br>";
     }
+
+    include "functions.php";
+
+    hello($firstName, $lastName, 1);
+    hello("Jean", $lastName, 0);
+    echo somme(1,3)
+
     ?>
