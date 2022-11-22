871c3f0eea68f4fe77c6efba8f4a21a0e85c9295
middleware <malifarge.thomas9@gmail.com>
2022-11-22 17:11:39 +0100

    try connect + print

--- bddConnexion.php
+++ bddConnexion.php
@@ -1,7 +1,25 @@
 <?php
     $user = 'root';
-    
+
     include "passwordBDD.php";
 
-    $dbh = new PDO('mysql:host=localhost;dbname=bdd-php-test',$user,$password);
+    try{
+        $dbh = new PDO('mysql:host=localhost;dbname=bdd-php-test',$user,$password);
+
+        $requete = 'SELECT * from user';
+
+        foreach($dbh->query($requete)as $row){
+            printRow($row);
+        }
+
+        $dbh = null;
+
+    }catch (PDOException $e){
+        print "Erreur!:" .$e->getMessage()."<br>";
+        die();
+    }
+
+    function printRow($row){
+        echo 'name : ' . $row['firstName'] . " " . $row['lastName'] . "<br> age : " . $row["age"] . "<br>";
+    }
 ?>
